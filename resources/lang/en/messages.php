8f99df175973322365987e6bd3d85f18d891ac24
= <=>
2019-03-03 22:10:27 +0700

    file

---
+++ resources/lang/en/messages.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: TuanTran
+ * Date: 3/3/2019
+ * Time: 10:00 PM
+ */
+return [
+  'title' => 'Message was written by english.'
+];
---
+++ resources/lang/vi/messages.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: TuanTran
+ * Date: 3/3/2019
+ * Time: 10:02 PM
+ */
+return [
+  'title' => 'Tin nhắn được viết bằng tiếng việt.'
+];
